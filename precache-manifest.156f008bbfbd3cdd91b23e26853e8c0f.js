3172acdaffcfb22acab8dd3813b733f903abc1bc
ilhaifa@yandex.ru <ilhaifa@yandex.ru>
2020-09-01 20:09:15 -0500

    Updates

---
+++ precache-manifest.156f008bbfbd3cdd91b23e26853e8c0f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5932664485c8327d020a2e3e4bd470d",
+    "url": "/shoppies-omdb-api/index.html"
+  },
+  {
+    "revision": "b33c4864d04ae369be9d",
+    "url": "/shoppies-omdb-api/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f9405d92d254808efbe6",
+    "url": "/shoppies-omdb-api/static/js/2.77d50249.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/shoppies-omdb-api/static/js/2.77d50249.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b33c4864d04ae369be9d",
+    "url": "/shoppies-omdb-api/static/js/main.9cc0a24e.chunk.js"
+  },
+  {
+    "revision": "9c0749935c87c3e873c1",
+    "url": "/shoppies-omdb-api/static/js/runtime-main.be94effe.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/shoppies-omdb-api/static/media/logo.5d5d9eef.svg"
+  }
+]);
