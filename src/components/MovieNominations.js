5cdaa867e932a8db2d51b37efe32f7279db0a36a
ilhaifa@yandex.ru <ilhaifa@yandex.ru>
2020-09-01 23:45:58 -0500

    2020-09-01

---
+++ src/components/MovieNominations.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export class MovieNominations extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Nominations</h1>
+      </div>
+    );
+  }
+}
+
+export default MovieNominations;
---
+++ src/components/MovieResult.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export class MovieResult extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Result for</h1>
+      </div>
+    );
+  }
+}
+
+export default MovieResult;
