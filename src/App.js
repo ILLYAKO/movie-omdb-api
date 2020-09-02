ff29ee52ad2ca3da705133ec74782fc96b073f90
ilhaifa@yandex.ru <ilhaifa@yandex.ru>
2020-09-02 15:40:37 -0500

    2020-09-02

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import React, { Component } from "react";
-import MovieSearchForm from "./components/MovieSearchForm"
-import MovieResult from "./components/MovieResult"
-import './App.css';
+import MovieSearchForm from "./components/MovieSearchForm";
+import MovieResult from "./components/MovieResult";
+import "./App.css";
 import MovieNominations from "./components/MovieNominations";
 
 class App extends Component {
-  render() { return (
-    <div className="App">
-      <h1>The Shoppies</h1>
-      <MovieSearchForm/>
-      <MovieResult/>
-      <MovieNominations/>
-
-    </div>
-  );
-}}
+  render() {
+    return (
+      <div className="App">
+        <h1>The Shoppies</h1>
+        <MovieSearchForm />
+        <MovieResult />
+        <MovieNominations />
+      </div>
+    );
+  }
+}
 
 export default App;
