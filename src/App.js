a1fa84acf59228b8b4be0f8f4037df0c2059fd53
ilhaifa@yandex.ru <ilhaifa@yandex.ru>
2021-09-29 13:04:12 -0400

    changed title

--- src/App.js
+++ src/App.js
@@ -70,7 +70,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>The Shoppies</h1>
+        <h1>Movie Catalog</h1>
         <MovieSearchForm
           onSearcMovieName={this.movieNamehandler}
           serverResponse={this.state.serverResponse}
